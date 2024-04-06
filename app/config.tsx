367616833ee118a6b06c5349b77464e8c4dccaa2
scan-dev <scan@vmlab.be>
2024-04-06 13:43:32 +0200

    update 24-04-06

--- app/config.tsx
+++ app/config.tsx
@@ -6,15 +6,15 @@
 
 export const config = {
     useOllamaInference: false,
-    useOllamaEmbeddings: false,
-    inferenceModel: 'mixtral-8x7b-32768', // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
-    inferenceAPIKey: process.env.GROQ_API_KEY, // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
-    embeddingsModel: 'text-embedding-3-small', // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
-    textChunkSize: 1000, // Recommended to decrease for Ollama
-    textChunkOverlap: 400, // Recommended to decrease for Ollama
+    useOllamaEmbeddings: true,
+    inferenceModel: 'gpt-3.5-turbo', // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
+    inferenceAPIKey: process.env.OPENAI_API_KEY, // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
+    embeddingsModel: 'nomic-embed-text', // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
+    textChunkSize: 750, // Recommended to decrease for Ollama
+    textChunkOverlap: 250, // Recommended to decrease for Ollama
     numberOfSimilarityResults: 4, // Numbher of similarity results to return per page
-    numberOfPagesToScan: 10, // Recommended to decrease for Ollama
-    nonOllamaBaseURL: 'https://api.groq.com/openai/v1', //Groq: https://api.groq.com/openai/v1 // OpenAI: https://api.openai.com/v1 
+    numberOfPagesToScan: 5, // Recommended to decrease for Ollama
+    nonOllamaBaseURL: 'https://api.openai.com/v1', //Groq: https://api.groq.com/openai/v1 // OpenAI: https://api.openai.com/v1 
 
     // Set LAN GPU server, example: http://192.168.1.100:11434/v1
     ollamaBaseUrl: process.env.OLLAMA_BASE_URL || 'http://localhost:11434/v1',
